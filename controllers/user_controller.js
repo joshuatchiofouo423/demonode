c6e468ccf8bacdeb7f8c10050bb58ca57a436b52
arielleaura23 <abomoarielle43@gmail.com>
2024-11-02 12:24:45 +0100

    initial

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,4 +1,5 @@
 const db = require('../models');
+const {Sequelize } = require('../models'); // Assurez-vous que Sequelize est importé ici
 
 
 exports.createdUser = async (req, res) => {
@@ -29,3 +30,29 @@ exports.getAllUser = async (req , res) => {
     const AllUser = await db.User.findAll();
     res.status(200).json({user: AllUser , message: 'Uilisateur recuperer'})   
 }
+exports.getUser = async (req, res) => {
+  try {
+    const { query } = req.body;
+
+    if (!query) {
+      
+      return res.status(400).json({ message: 'Veuillez entrer un nom ou un prénom pour obtenir des suggestions.' });
+    }
+
+    const suggestions = await db.User.findAll({
+      where: {
+        [Sequelize.Op.or]: [
+          { nom: { [Sequelize.Op.like]: `%${query}%` } },
+          { prenom: { [Sequelize.Op.like]: `%${query}%` } }
+        ]
+      }
+    });
+    if (suggestions.length === 0) {
+      return res.status(404).json({ message: 'Aucun utilisateur trouvé.' });
+    }
+    res.status(200).json(suggestions);
+  } catch (error) {
+    console.error('Erreur lors de la récupération des suggestions:', error);
+    res.status(500).json({ message: 'Une erreur est survenue lors de la récupération des suggestions.' });
+  }
+};
--- routes/user_route.js
+++ routes/user_route.js
@@ -4,6 +4,8 @@ const usernController = require('../controllers/user_controller');
 
 router.post('/creates', usernController.createdUser);
 router.get('/get-all-user', usernController.getAllUser);
+router.post('/get-user', usernController.getUser);
+
 
 
 
